ec01a627caaa6ae84254459b977bac17242a9c8a
John Rhay Domingo <jhnrhyyyyyyyyyy@gmail.com>
2024-10-08 11:23:20 +0800

    Switch Statement Activity

--- taskCalendar.js
+++ taskCalendar.js
@@ -1,27 +1,30 @@
-let day = prompt("Enter a day of  week:"); 
+function taskCalendar(){
 
-switch (day.toLowerCase()){
+    let day = prompt("Enter a day of  week:"); 
+
+    switch (day.toLowerCase()){
     case "monday":
-        console.log("Your task for Monday is: Finish your assignment.");
+        alert("Your task for Monday is: Finish your assignment.");
         break;
     case "tuesday":
-        console.log("Your task for Tuesday is: Finish your projects.");
+        alert("Your task for Tuesday is: Finish your projects.");
         break;
     case "wednesday":
-        console.log("Your task for Wednesday is: Complete the project report.");
+        alert("Your task for Wednesday is: Complete the project report.");
             break;
     case "thursday":
-        console.log("Your task for Thursday is: Finish your activities.");
+        alert("Your task for Thursday is: Finish your activities.");
         break;      
     case "friday":
-        console.log("Your task for Friday is: Prepare for the presentation.");
+        alert("Your task for Friday is: Prepare for the presentation.");
         break;
     case "saturday":
-        console.log("Your task for Saturday is: Finish your household chores.");
+        alert("Your task for Saturday is: Finish your household chores.");
         break;
     case "sunday":
-        console.log("Your task for Sunday is: It's time to review.");
+        alert("Your task for Sunday is: It's time to review.");
         break;
     default:
-        console.log("Invalid Input!");
+        alert("Invalid Input!");
+    }
 }
